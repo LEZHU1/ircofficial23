cc3b4dbdfe296f9e6ebe8d58a5a67cff6f46bd91
Leilani Zhu <leilanizhu@Leilanis-MacBook-Air.local>
2023-11-14 17:05:02 -0800

    auto

---
+++ src/main/java/frc/robot/commands/AutoDrive.java
@@ -0,0 +1,29 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.subsystems.Drivetrain;
+
+public class AutoDrive extends CommandBase {
+    Drivetrain drivetrain;
+    Timer timer;
+    double leftspeed, rightspeed;
+    double time;
+
+    public AutoDrive(Drivetrain dt, double ls, double rs, double t) {
+drivetrain = dt;
+leftspeed = ls;
+rightspeed = rs;
+time = t;
+    }
+    public void initialize(){
+        timer.reset();
+        timer.start();
+    }
+    public void execute(){
+        drivetrain.drive(leftspeed, rightspeed);
+    }
+    public void end(){
+
+    }
+}
