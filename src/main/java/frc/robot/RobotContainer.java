83c36112a0a000a6821b1df845e97ef6a501cded
LEZHU1 <143223104+LEZHU1@users.noreply.github.com>
2023-11-14 17:11:59 -0800

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,17 +4,36 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.AutoDrive;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Drivetrain;
+
 
 public class RobotContainer {
+  Joystick leftJoystick = new Joystick(0);
+  Joystick rightJoystick = new Joystick(1);
+  JoystickButton raiseArmbtn = new JoystickButton(leftJoystick, 2);
+  JoystickButton lowerArmBtn = new JoystickButton(rightJoystick,  2);
+
+  Drivetrain drivetrain = new Drivetrain();
+  Arm arm = new Arm();
+
   public RobotContainer() {
+    drivetrain.setDefaultCommand(new InstantCommand(() -> drivetrain.drive(leftJoystick.getY(), rightJoystick.getY())));
+
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    raiseArmbtn.onTrue(new InstantCommand(() -> arm.moveArm(0.5)));
+    lowerArmBtn.onTrue(new InstantCommand(() -> arm.moveArm(-0.5)));
+  }
 
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+    return new AutoDrive(drivetrain,0.5 ,0.5 ,2);
   }
 }
